871323b899ba2f71a814d12bac814761719a8915
Li Blissz <liblissz3@gmail.com>
2026-01-04 01:39:33 +0100

    commit

--- src/Chats/components/ChatMain/MessageBubble.jsx
+++ src/Chats/components/ChatMain/MessageBubble.jsx
@@ -1,46 +1,5 @@
 
 
-// import React from 'react';
-
-// function MessageBubble({ message, type }) {
-//     return (
-//         <div className={`ngn-message-bubble ngn-message-bubble--${type}`}>
-//             <div className="ngn-message-bubble__text">
-//                 {message.text}
-//             </div>
-
-//             {console.log("image", message.image)}
-//             {message.image && (
-
-//                 <img
-//                     src={message.image}
-//                     alt="message attachment"
-//                     style={{
-//                         maxWidth: '200px',
-//                         maxHeight: '200px',
-//                         borderRadius: '8px',
-//                         display: 'block',
-//                         marginTop: '4px',
-//                     }}
-//                 />
-//             )}
-
-//             <div className="ngn-message-bubble__meta">
-//                 <span className="ngn-message-bubble__time">
-//                     {new Date(message.createdAt).toLocaleTimeString([], {
-//                         hour: '2-digit',
-//                         minute: '2-digit',
-//                     })}
-//                 </span>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default MessageBubble;
-
-
-
 import React from 'react';
 
 function MessageBubble({ message, type }) {
@@ -72,7 +31,7 @@ function MessageBubble({ message, type }) {
                         src={message.video}
                         controls
                         style={{
-                            maxWidth: '250px',
+                            maxWidth: '150px',
                             borderRadius: '8px',
                             display: 'block',
                         }}
